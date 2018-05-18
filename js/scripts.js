378f2c350709001300917c44675b4b07c25dd310
lus <lusinehovh@gmail.com>
2018-05-18 13:05:29 +0400

    finished but not finalized

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,127 @@
-$( document ).ready(function() {
+$(document).ready(function() {
+window.score = 0;
+window.success = 0;
+window.flippedCard = null;
+window.flippedType = null;
+window.clickable = true;
+window.cards = [
+	{ dataType:"valet", dataId:"1" },
+	{ dataType:"valet", dataId:"2" },
+	{ dataType:"man", dataId:"3" },
+	{ dataType:"man", dataId:"4" },
+	{ dataType:"queen", dataId:"5" },
+	{ dataType:"queen", dataId:"6" },
+	{ dataType:"eagle", dataId:"7" },
+	{ dataType:"eagle", dataId:"8" },
+	{ dataType:"flag", dataId:"9" },
+	{ dataType:"flag", dataId:"10" },
+	{ dataType:"dog", dataId:"11" },
+	{ dataType:"dog", dataId:"12" }
+];
+var h1 = document.getElementsByTagName('h2')[0], seconds = 0, minutes = 0, hours = 0, timeout;
 
-	var clicked = [];
-	var score = 0;
-	var success = 0;
-
-	var cards = [
-		{ dataType:"valet", dataId:"1" },
-		{ dataType:"valet", dataId:"2" },
-		{ dataType:"man", dataId:"3" },
-		{ dataType:"man", dataId:"4" },
-		{ dataType:"queen", dataId:"5" },
-		{ dataType:"queen", dataId:"6" },
-		{ dataType:"eagle", dataId:"7" },
-		{ dataType:"eagle", dataId:"8" },
-		{ dataType:"flag", dataId:"9" },
-		{ dataType:"flag", dataId:"10" },
-		{ dataType:"dog", dataId:"11" },
-		{ dataType:"dog", dataId:"12" }
-	];
-
-	cards = cards.sort(function(a, b){return 0.5 - Math.random()});
-	
-	for (var i = 0; i < cards.length; i++) {
-		var currentId = cards[i]["dataId"];
-		var currentType = cards[i]["dataType"];
-		$('.card-row').append('<div class="card" class="col-3"><img src="images/main.jpg" id="'+currentId+'" data-type="'+currentType+'" class="img-fluid back" width="150"><img src="images/'+currentType+'.jpg" class="img-fluid front" width="150"></div>');
+function startNewGame(arr) {
+	arr = arr.sort(function(a, b){return 0.5 - Math.random()});
+	for (var i = 0; i < arr.length; i++) {
+		var currentId = arr[i]["dataId"];
+		var currentType = arr[i]["dataType"];
+		$('.card-row').append('<div id="'+currentId+'" data-type="'+currentType+'" class="col-3 img-card"><img src="images/main.jpg" class="img-fluid back" width="150"><img src="images/'+currentType+'.jpg" class="img-fluid front" width="150"></div>');
 	}
+	timer();
+}
 
-	$('.back').click(function(){
-		$(this).hide().next().show();
-    	score += 1;
-    	clicked.push($(this).attr('data-type'));
-    	var $this = $(this);
+startNewGame(cards);
 
-    	setTimeout(function(){ 
-    		if (clicked[clicked.length-1] != clicked[clicked.length-2]) {
-    		$this.show().next().hide();
-    		// alert($this.attr("data-type"));
-    		}
-	 	}, 2000);
 
-	});
+function openCard(cardId) {
+	$('#' + cardId + ' .back').css('display', 'none').next().show();
+}
+
+function closeCards(cardId1, cardId2) {
+	$('#' + cardId1 + ' .front, #' + cardId2 + ' .front' ).css('display', 'none').prev().show();
+	flippedCard = null;
+	flippedType = null;
+	clickable = true;
+}
+
+function addTime() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+    
+    h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+    timer();
+}
+
+function clearTime() {
+	h1.textContent = "00:00:00";
+    seconds = 0; minutes = 0; hours = 0;
+}
+
+function timer() {
+    timeout = setTimeout(addTime, 1000);
+}
+
+function endGame(time) {
+	var message = "Your Time Is " + time;
+	alert(message);
+	$('.header').show();
+}
+
+function replayGame() {
+	$('.header').hide();
+	$('.card-row').html("");
+	score = 0;
+	success = 0;
+	flippedCard = null;
+	flippedType = null;
+	clickable = true;
+	h1 = document.getElementsByTagName('h2')[0], seconds = 0, minutes = 0, hours = 0, timeout;
+	startNewGame(cards);
+}
+
+$(document).on("click", ".replay", function() {
+	replayGame();
+});
+
+$(document).on("click",".img-card",function(){
+	if (!clickable) {
+		return;
+	} 
+	if (!flippedCard) {
+		flippedCard = $(this).attr('id');
+		flippedType = $('#' + flippedCard).attr('data-type');
+		openCard(flippedCard);
+	}else{
+		var currentCard = $(this).attr('id');
+		var currentType = $('#' + currentCard).attr('data-type');
+		openCard(currentCard);
+		if (flippedType === currentType) {
+			success++;
+			flippedCard = null;
+			flippedType = null;
+		} else {			
+			clickable = false;
+			setTimeout(function() {
+				closeCards(flippedCard, currentCard);
+			}, 1000);
+		}
+	}
+	if (success === 6) {
+		var timePassed = h1.textContent;
+		clearTimeout(timeout);
+		clearTime();
+		setTimeout(function() {
+			endGame(timePassed);
+		},1200);
+		
+	}
+});
 });
